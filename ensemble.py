8c1a856795ef6b2f357cb2e0d684b028baec8d0d
zvq211 <zvq211@hendrixgate03fl.unicph.domain>
2025-04-30 18:09:04 +0200

    end-to-end ensembles?

--- ensemble.py
+++ ensemble.py
@@ -1,8 +1,32 @@
 #!/usr/bin/env python3
+import re
 import pickle
 from pathlib import Path
+import numpy as np
 
-def select_indicies(folder, mode="all"):
+def group_strs(strs):
+    """Groups identity strs by the first number in the string"""
+
+    ss = [(s, re.search("\d+", s)[0]) for s in strs]
+    ss = sorted(ss, key=lambda x: int(x[1]))
+
+    res = []
+    acc = []
+    current_n = "0"
+    for s, n in ss:
+        if n == current_n:
+            acc.append(s)
+        else:
+            current_n = n
+            res.append(acc)
+            acc = []
+            acc.append(s)
+    res.append(acc)
+
+    return np.array(res)
+
+
+def select_indicies(folder, mode="all", dataset="dataset.pkl"):
     """ Select a subset of model checkpoints from a folder """
     candidate_strs = Path(folder).glob("*.h5")
     candidate_strs = [p.name for p in candidate_strs]
@@ -14,14 +38,40 @@ def select_indicies(folder, mode="all"):
         with open(path, "rb") as f:
             return pickle.load(f)
 
+    def best_model_idx(preds, truth):
+        """
+        Given a sequence of predictions, evaluate for
+        accuracy against y_true, return the best
+        """
+        accs = [
+            sum([
+                (t > .5) == (p > .5)
+                for (t,p) in zip(truth, pred)
+            ]) / len(pred) for pred in preds
+        ]
+        return accs.index(max(accs))
+
     match mode:
         case "all":
             return [s for (s, _, _) in candidate_strs]
         case "best":
-            y_val = 0
-            candidate_vals = [model_to_val_file(s) for s in candidate_strs]
-            # stack, compare for accuracy, mean axis=1, argmax
-        case "last":
-            pass
+            y_val = pickle.load(open(folder / dataset, "rb"))["y_val"]
 
-select_indicies(Path("moon_models"), "best")
+            candidate_strs = group_strs(candidate_strs)
+            val_preds = [[model_to_val_file(s) for s in ss] for ss in candidate_strs]
+            best_idxs = [best_model_idx(model_preds, y_val) for model_preds in val_preds]
+
+            offsets = np.arange(len(best_idxs)) * len(val_preds[0])
+            return offsets + np.array(best_idxs)
+
+        case "last":
+            candidate_strs = group_strs(candidate_strs)
+            n_models, n_checkpoints = np.array(candidate_strs).shape
+            return np.arange(1,n_models+1) * n_checkpoints - 1
+
+def ensemble_predictions(preds, weights):
+    return np.array(preds.flat) * weights
+
+def generate_rho_weights():
+
+select_indicies(Path("moon_models"), "last")
--- majority_vote_bounds.py
+++ majority_vote_bounds.py
@@ -1,57 +1,159 @@
-#!/usr/bin/env python3
+#
+# Implementation of the lambda bound and optimization procedure.
+#
+# Based on paper:
+# [Niklas Thiemann, Christian Igel, Olivier Wintenberger, and Yevgeny Seldin.
+#  A strongly quasiconvex385PAC-Bayesian bound. InAlgorithmic Learning Theory (ALT), 2017]
+#
 import numpy as np
-from sklearn.utils import check_random_state
 
-from mvb import NeuralNetworkPostTrainClassifier as EnsembleClassifier
+from math import ceil, log, sqrt, exp
 
-def optimize(dataset, m, smode, opt, reps, inpath, ensemble_path, write_files, indices=None, test_risk_indices=None, test_bound_indices=None, test_pred_file_name='test_predictions.pkl'):
-    results = []
 
-    X,Y = dataset
-    trainX, valX, testX = X
-    trainY, valY, testY = Y
+def uniform_distribution(m):
+    return (1.0 / m) * np.ones((m,))
 
-    first_rho = None
 
-    C = np.unique(trainY).shape[0]
-    n = (trainX.shape[0], testX.shape[0], trainX.shape[1], C)
+def kl(rho, pi):
+    m = pi.shape[0]
+    assert rho.shape[0] == m
+    kl = 0.0
+    for h in range(m):
+        kl += rho[h] * log(rho[h] / pi[h]) if rho[h] > 10**-12 else 0
+    return kl
 
-    rf = EnsembleClassifier(m, ensemble_path=ensemble_path, indices=indices, test_risk_indices=test_risk_indices, test_bound_indices=test_bound_indices, test_pred_file_name=test_pred_file_name)
 
-    rhos = []
-    rf.fit(trainX, trainY, valX, valY)
+def softmax(dist):
+    dexp = np.exp(dist)
+    return dexp / np.sum(dexp, axis=0)
 
-    _, mv_risk = rf.predict(testX,testY)
-    stats = rf.stats()
-    bounds, stats = rf.bounds(stats=stats)
-    res_unf = (mv_risk, stats, bounds, -1, -1)
 
-    # Optimize Lambda
-    (_, rho, bl) = rf.optimize_rho('Lambda')
-    _, mv_risk = rf.predict(testX,testY)
-    stats = rf.aggregate_stats(stats)
-    bounds, stats = rf.bounds(stats=stats)
-    res_lam = (mv_risk, stats, bounds, bl, -1)
-    rhos.append(rho)
+def iRProp(grad, func, x0,
+        max_iterations=1000,
+        eps=10**-9,
+        step_init=0.1,
+        step_min=10^-20,
+        step_max=10**5,
+        inc_fact=1.1,
+        dec_fact=0.5
+           ):
+    """Resilient backpropagation"""
+    n    = x0.shape[0]
+    dx   = np.zeros((max_iterations, n))
+    x    = np.zeros((max_iterations, n))
+    x[0] = x0
+    step = np.ones(n)*step_init
 
-    # Optimize TND
-    (_, rho, bl) = rf.optimize_rho('TND', options={'optimizer':opt})
-    _, mv_risk = rf.predict(testX,testY)
-    stats = rf.aggregate_stats(stats)
-    bounds, stats = rf.bounds(stats=stats)
-    res_mv2 = (mv_risk, stats, bounds, bl, -1)
-    rhos.append(rho)
+    fx   = np.ones(max_iterations)
+    fx[0]= func(x[0])
+    tb   = 0
 
-    # Optimize DIS if binary
-    if(C==2):
-        (_, rho, bl, bg) = rf.optimize_rho('DIS', options={'optimizer':opt})
-        _, mv_risk = rf.predict(testX,testY)
-        stats = rf.aggregate_stats(stats)
-        bounds, stats = rf.bounds(stats=stats)
-        res_mv2u = (mv_risk, stats, bounds, bl, bg)
-        rhos.append(rho)
-    else:
-        res_mv2u = (-1.0, dict(), dict(), -1, -1)
-        rhos.append(-np.ones((m,)))
+    t = 1
+    while t < max_iterations:
+        delta = fx[t-1]-fx[t-2] if t>1 else -1.0
+        if t-tb > 10:
+            break
+        dx[t] = grad(x[t-1])
 
-    return first_rho, results
+        # Update set size
+        det = np.multiply(dx[t], dx[t-1])
+        # Increase where det>0
+        step[det>0] = step[det>0]*inc_fact
+        # Decrease where det<0
+        step[det<0] = step[det<0]*dec_fact
+        # Upper/lower bound by min/max
+        step        = step.clip(step_min, step_max)
+
+        # Update w
+        # If det >= 0, same as RProp
+        x[t][det>=0] = x[t-1][det>=0] - np.multiply(np.sign(dx[t]), step)[det>=0]
+        # If func(x[t-1])>func(x[t-2]) set x[t] to x[t-2] where det<0 (only happens if t>1, as det==0 for t=1)
+        if delta>0:
+            x[t][det<0] = x[t-2][det<0]
+        else:
+            x[t][det<0] = x[t-1][det<0] - np.multiply(np.sign(dx[t]), step)[det<0]
+        # Reset dx[t] = 0 where det<0
+        dx[t][det<0] = 0
+
+        # Compute func value
+        fx[t] = func(x[t])
+        if fx[t] < fx[tb]:
+            tb = t
+
+        t += 1
+    return x[tb]
+
+
+def optimizeTND(tandem_risks, n, delta=0.05):
+    options = dict() if options is None else options
+    max_iterations = options.get('max_iterations', 100)
+    eps = options.get('eps', 10**-9)
+
+    m   = tandem_risks.shape[0]
+    rho = uniform_distribution(m)
+    pi  = uniform_distribution(m)
+
+    # Some helper functions
+    def _tndr(rho): # Compute tandem risk from tandem risk matrix and rho
+        return np.average(np.average(tandem_risks, weights=rho, axis=0), weights=rho)
+
+    def _bound(rho, lam=None): # Compute value of bound (also optimize lambda if None)
+        rho  = softmax(rho)
+        tndr = _tndr(rho)
+        KL   = kl(rho,pi)
+        if lam is None:
+            lam = 2.0 / (sqrt((2.0*n*tndr)/(2.0*KL+log(2.0*sqrt(n)/delta)) + 1) + 1)
+        bound = tndr / (1.0 - lam/2.0) + (2.0*KL+log(2.0*sqrt(n)/delta))/(lam*(1.0-lam/2.0)*n)
+        return (bound, lam)
+
+    # 1st order methods
+    def _gradient(rho, lam): # Gradient (using rho = softmax(rho'))
+        Srho = softmax(rho)
+        # D_jS_i = S_i(1[i==j]-S_j)
+        Smat = -np.outer(Srho, Srho)
+        np.fill_diagonal(Smat, np.multiply(Srho,1.0-Srho))
+        return np.dot(2*(np.dot(tandem_risks,Srho)+1.0/(lam*n)*(1+np.log(Srho/pi))),Smat)
+
+    def _optRho(rho, lam):
+        return iRProp(lambda x: _gradient(x,lam), lambda x: _bound(x,lam)[0], rho,\
+                eps=eps, max_iterations=max_iterations)
+
+    b, lam   = _bound(rho)
+    bp       = b+1
+    while abs(b-bp) > eps:
+        bp = b
+        # Optimize rho
+        nrho = _optRho(rho, lam)
+        b, nlam = _bound(nrho)
+        if b > bp:
+            b = bp
+            break
+        rho, lam = nrho, nlam
+
+    return (min(1.0,4*b), softmax(rho), lam)
+
+
+def tandem_risks(predictions, target):
+    """
+    given a (model, predictions) for all of list target,
+    produce a n x n array of risks, where n = len(target)
+    """
+    n = len(target)
+    risks = np.zeros((n,n))
+
+    for i, p_a in enumerate(predictions):
+        for j, p_b in enumerate(predictions):
+            risks[i,j] += np.sum(
+                np.logical_and(
+                    p_a != target,
+                    p_b != target
+                )
+            )
+
+    return risks / n
+
+
+def optimize_rho(predictions, target):
+    risks = tandem_risks(predictions, target)
+    (bound, rho, lam) = optimizeTND(risks, len(target))
+    return (bound, rho, lam)
