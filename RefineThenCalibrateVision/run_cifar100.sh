61733296562b7cf52e8728ae4d5559f03f65a4b3
zvq211 (by proxy) <zvq211@hendrix>
2025-05-03 10:20:18 +0200

    Moved refine cv folder

---
+++ RefineThenCalibrateVision/run_cifar100.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --time=24:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --exclude=hendrixgpu06fl,hendrixgpu09fl,hendrixgpu10fl,hendrixgpu17fl,hendrixgpu18fl,hendrixgpu26fl
+
+source ~/pac-bayes/venv/bin/activate
+python main.py \
+    --run_name run_${SLURM_ARRAY_TASK_ID}_cifar100 \
+    --seed ${SLURM_ARRAY_TASK_ID} \
+    --seeds_per_job 10 \
+    --dataset cifar100 \
+    --folder cifar100_models \
+    --project "PAC-Bayes NeurIPS 2025"
