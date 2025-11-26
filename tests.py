08e97429b5e2ddeef62d6a0d616ecb5eaa424766
GustavMH <45123074+GustavMH@users.noreply.github.com>
2025-11-26 12:01:17 +0100

    Move utilities to util.py

--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def test_oob_tandem_risk():
-    oob = oob_tandem_risks(
+    calc_res, calc_n2 = oob_tandem_risks(
         np.ones((3,100)),
         np.block([
             [np.arange(100)],
@@ -13,4 +13,39 @@ def test_oob_tandem_risk():
             np.ones(100)
         ])
     )
-    print(oob)
+    res = np.array([[1.        , 1.        , 1.        ],
+                    [1.        , 0.7       , 0.57142857],
+                    [1.        , 0.57142857, 0.4       ]])
+    n2 = 40
+    return (calc_res-res).sum() , calc_n2 == n2
+
+def reference_oob_tandem_risks(preds, targs):
+    m = len(preds)
+    tandem_risks  = np.zeros((m,m))
+    n2            = np.zeros((m,m))
+
+    for i in range(m):
+        (M_i, P_i) = preds[i]
+        for j in range(i, m):
+            (M_j, P_j) = preds[j]
+            M = np.multiply(M_i,M_j)
+            tandem_risks[i,j] = np.sum(np.logical_and(P_i[M==1]!=targs[M==1], P_j[M==1]!=targs[M==1]))
+            n2[i,j] = np.sum(M)
+
+            if i != j:
+                tandem_risks[j,i] = tandem_risks[i,j]
+                n2[j,i]           = n2[i,j]
+
+    return tandem_risks, n2
+
+reference_oob_tandem_risks(
+    np.block([
+        [[np.ones(100),np.zeros(60)],
+         [np.ones(100),np.zeros(60)]],
+        [[np.zeros(30),np.ones(100),np.zeros(30)],
+         [np.zeros(30),np.ones(100),np.zeros(30)]],
+        [[np.zeros(60),np.ones(100)],
+         [np.zeros(60),np.ones(100)]]
+    ]),
+    np.block([np.zeros(100), np.ones(60)])
+)
---
+++ util.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+def oob_tandem_risks(
+        preds: [np.array],
+        pred_idx: [np.array],
+        targs: np.array
+):
+    """
+    Calculate a (model, model) size array of risk scores.
+
+    Predictions from an out-of-bag (unused for optimization) validation
+    set; the PREDS array has shape (model, prediction), the TARGS array
+    contains the dataset from which the validation set has been drawn
+    """
+    assert(len(preds) == len(pred_idx))
+    assert(all(p.shape == i.shape for (p,i) in zip(preds, pred_idx)))
+
+    m = len(preds)
+    tandem_risks = np.zeros((m,m))
+    n_intersects = np.zeros((m,m))
+
+    for i, (preds_a, idx_a) in enumerate(zip(preds, pred_idx)):
+        for j, (preds_b, idx_b) in islice(enumerate(zip(preds, pred_idx)), i, m):
+            idxs, c_a, c_b = np.intersect1d(idx_a, idx_b, True, True)
+
+            risk = np.sum(
+                np.logical_and(
+                    preds_a[c_a] != targs[idxs],
+                    preds_b[c_b] != targs[idxs]
+                )
+            )
+            tandem_risks[i,j] = risk
+            tandem_risks[j,i] = risk
+
+            n = len(idxs)
+            n_intersects[i,j] = n
+            n_intersects[j,i] = n
+
+    return tandem_risks / n_intersects, n_intersects.min()
+
+
+
+def oob_gibbs_risks(preds, pred_idx, targs):
+    """
+    Calculate a (model) size array of risk scores.
+
+    Predictions from an out-of-bag (unused for optimization) validation
+    set; the PREDS array has shape (model, prediction), the TARGS array
+    contains the dataset from which the validation set has been drawn
+    """
+    assert(len(preds) == len(pred_idx))
+    assert(all(p.shape == i.shape for (p,i) in zip(preds, pred_idx)))
+
+    m = len(preds)
+    risks = np.zeros(m)
+    n_intersects = np.zeros(m)
+
+    for i, (preds, idxs) in enumerate(zip(preds, pred_idx)):
+        risks[i] = np.sum(preds != targs[idxs])
+        n_intersects[i] = len(idxs)
+
+    return risks / n_intersects, n_intersects.min()
+
+
+def split_bootstrap(
+        rng: np.random.RandomState,
+        X: np.array,
+        Y: np.array,
+        n_samples: int,
+        n_classes: None | int
+) -> ((np.array, np.array), (np.array, np.array)):
+    """Sample points (x,y) in (X,Y) w. replacement, get at least one example per class in Y.
+    Return the sample and sample indicies and out-of-bag samples and indicies"""
+    n_classes = n_classes if (n_classes is None) else len(np.unique(Y))
+    Y_sample = np.zeros((n_samples, *Y.shape[1:]))
+
+    while not len(np.unique(Y_sample)) == n_classes:
+        sample_idx = rng.integers(n_samples, size=n_samples)
+        Y_sample = Y[sample_idx]
+
+    oob_idx = np.delete(np.arange(len(X)), sample_idx)
+
+    return sample_idx, oob_idx
