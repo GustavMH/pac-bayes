27439c249ac7ceb77796535a9704e3bff95f44ed
zvq211 <zvq211@hendrixgate03fl.unicph.domain>
2026-05-19 13:51:58 +0200

    merge

--- models/resnet.py
+++ models/resnet.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
-from torchvision.models import resnet18
 
-def resnet18_n_cat(n_cats=10):
-    model = resnet18("IMAGENET1K_V1")
-    model.fc = nn.Linear(mlp.fc.in_features, n_cats)
+from torchvision.models import resnet18 as RN18
+import torch.nn as nn
+
+def resnet18(n_cats=10, weights="IMAGENET1K_V1"):
+    mlp = RN18(weights="IMAGENET1K_V1")
+    mlp.fc = nn.Linear(mlp.fc.in_features, n_cats)
+    return mlp
